2edc58479c3d46bd68826b571426faddbc5756ff
Vinicius Rodrigues <vinicius.rodrigues@gmail.com>
2019-11-18 22:42:04 -0300

    Utilizando UserDTO para retornar os dados em /users~

--- src/main/java/com/varod/springbootmongodb/resources/UserResource.java
+++ src/main/java/com/varod/springbootmongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.varod.springbootmongodb.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.varod.springbootmongodb.domain.User;
+import com.varod.springbootmongodb.dto.UserDTO;
 import com.varod.springbootmongodb.services.UserService;
 
 @RestController
@@ -19,7 +21,9 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
-		return ResponseEntity.ok().body(service.findAll());
+	public ResponseEntity<List<UserDTO>> findAll() {
+		List<User> users = service.findAll();
+		List<UserDTO> usersDto = users.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(usersDto);
 	}
 }
