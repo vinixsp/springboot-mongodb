9d542ee37b0da815225e7e551845e01ef83178b7
Vinicius Rodrigues <vinicius.rodrigues@gmail.com>
2019-11-19 17:33:37 -0300

    Obtendo um post por id

---
+++ src/main/java/com/varod/springbootmongodb/services/PostService.java
@@ -0,0 +1,25 @@
+package com.varod.springbootmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.varod.springbootmongodb.domain.Post;
+import com.varod.springbootmongodb.repository.PostRepository;
+import com.varod.springbootmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> post = repo.findById(id);
+		if (post.isEmpty()) {
+			throw new ObjectNotFoundException("Objeto não encontrado.");
+		}
+		return post.get();
+	}
+}
