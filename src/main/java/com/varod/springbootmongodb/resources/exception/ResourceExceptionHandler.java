a3591673fc087dd0b402dfa176d5ebecd74c4979
Vinicius Rodrigues <vinicius.rodrigues@gmail.com>
2019-11-18 23:21:28 -0300

    Obtendo o usuário por id

---
+++ src/main/java/com/varod/springbootmongodb/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.varod.springbootmongodb.resources.exception;
+
+import java.net.http.HttpRequest;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.varod.springbootmongodb.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(),
+				status.value(), 
+				"Não encontrado", 
+				e.getMessage(), 
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
--- src/main/java/com/varod/springbootmongodb/services/UserService.java
+++ src/main/java/com/varod/springbootmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.varod.springbootmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.varod.springbootmongodb.domain.User;
 import com.varod.springbootmongodb.repository.UserRepository;
+import com.varod.springbootmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,13 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		if (user.isEmpty()) {
+			throw new ObjectNotFoundException("Objeto não encontrado"); 
+		}
+		return user.get();
 	}
 }
