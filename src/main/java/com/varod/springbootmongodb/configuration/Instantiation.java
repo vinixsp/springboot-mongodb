d23f6daa547f58281b96f0c75743fe6f8dd1506a
Vinicius Rodrigues <vinicius.rodrigues@gmail.com>
2019-11-19 17:11:52 -0300

    Referenciando os posts do usuário

--- src/main/java/com/varod/springbootmongodb/configuration/Instantiation.java
+++ src/main/java/com/varod/springbootmongodb/configuration/Instantiation.java
@@ -40,6 +40,9 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2019"), "Partiu viagem!", "Vou viajar para São Paulo.\nAbraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("24/04/2019"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 
 }
