da5952e3ec9dd53871b756544aa4ad26008267c9
Vinicius Rodrigues <vinicius.rodrigues@gmail.com>
2019-11-19 17:12:57 -0300

    minor fix (lazy = true)

--- src/main/java/com/varod/springbootmongodb/domain/User.java
+++ src/main/java/com/varod/springbootmongodb/domain/User.java
@@ -17,7 +17,7 @@ public class User implements Serializable {
 	private String name;
 	private String email;
 	
-	@DBRef
+	@DBRef(lazy = true)
 	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
